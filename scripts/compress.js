64d8faa9d94ec254ae8520ef7d34c0bfe7903cfa
Jarred Sumner <jarred@jarredsumner.com>
2018-07-26 20:21:50 -0700

    Fix compress script

--- scripts/compress.js
+++ scripts/compress.js
@@ -1,27 +1,39 @@
-const fs = require('fs');
-const ChromeExtension = require('crx');
-/* eslint import/no-unresolved: 0 */
-const name = require('../build/manifest.json').name;
-const argv = require('minimist')(process.argv.slice(2));
+const fs = require("fs");
+const ChromeExtension = require("crx");
+const path = require("path");
 
-const keyPath = argv.key || 'key.pem';
+const RELEASES_PATH = path.join(__dirname, "../releases");
+const BUILD_PATH = path.join(__dirname, "../build");
+const manifest = require(path.join(BUILD_PATH, "manifest.json"));
+const name = manifest.name;
+const version = manifest.version;
+const argv = require("minimist")(process.argv.slice(2));
+
+const keyPath = argv.key || "key.pem";
 const existsKey = fs.existsSync(keyPath);
 const crx = new ChromeExtension({
-  appId: argv['app-id'],
+  appId: argv["app-id"],
   codebase: argv.codebase,
   privateKey: existsKey ? fs.readFileSync(keyPath) : null
 });
 
-crx.load('build')
+crx
+  .load(BUILD_PATH)
   .then(() => crx.loadContents())
-  .then((archiveBuffer) => {
-    fs.writeFile(`${name}.zip`, archiveBuffer);
+  .then(async archiveBuffer => {
+    const baseName = `${name}-${version}-${new Date().toISOString()}`;
+    const zipFileName = path.join(RELEASES_PATH, `${baseName}.zip`);
+    fs.writeFile(zipFileName, archiveBuffer, () =>
+      console.log(`Wrote ${zipFileName}`)
+    );
 
-    if (!argv.codebase || !existsKey) return;
-    crx.pack(archiveBuffer).then((crxBuffer) => {
-      const updateXML = crx.generateUpdateXML();
-
-      fs.writeFile('update.xml', updateXML);
-      fs.writeFile(`${name}.crx`, crxBuffer);
+    return crx.pack(archiveBuffer).then(async crxBuffer => {
+      const crxFilename = path.join(RELEASES_PATH, `${baseName}.crx`);
+      fs.writeFile(crxFilename, crxBuffer, () =>
+        console.log(`Wrote ${crxFilename}`)
+      );
     });
+  })
+  .catch(err => {
+    console.error(err);
   });

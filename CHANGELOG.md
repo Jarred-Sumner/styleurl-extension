46fd239c2d7930d95b6fa09459fb90590cd94942
Jarred Sumner <jarred@jarredsumner.com>
2018-07-27 15:46:20 -0700

    Update license prepare for release

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+## 1.0.4
+
+Bump version.
+
+## 1.0.3
+
+Bump version.
+
+## 1.0.2
+
+Initial Release.
--- scripts/compress.js
+++ scripts/compress.js
@@ -17,45 +17,89 @@ const crx = new ChromeExtension({
   privateKey: existsKey ? fs.readFileSync(keyPath) : null
 });
 
+const ghRelease = require("gh-release");
+
+const baseName = `${name}-${version}-${new Date().toISOString()}`;
+const crxFilename = path.join(RELEASES_PATH, `${baseName}.crx`);
+const zipFileName = path.join(RELEASES_PATH, `${baseName}.zip`);
+
 crx
   .load(BUILD_PATH)
   .then(() => crx.loadContents())
   .then(async archiveBuffer => {
-    const baseName = `${name}-${version}-${new Date().toISOString()}`;
-    const zipFileName = path.join(RELEASES_PATH, `${baseName}.zip`);
-    fs.writeFile(zipFileName, archiveBuffer, () => {
-      console.log(`Wrote ${zipFileName}`);
+    return new Promise((resolve, reject) => {
+      fs.writeFile(zipFileName, archiveBuffer, () => {
+        console.log(`Wrote ${zipFileName}`);
 
-      const CREDENTIALS_PATH = path.join(__dirname, "../credentials.json");
-      if (fs.existsSync(CREDENTIALS_PATH) && !!argv["release"]) {
-        console.log("Uploading to Chrome Store...");
-        const webStore = require("chrome-webstore-upload")({
-          ...require(CREDENTIALS_PATH),
-          extensionId: "emplcligcppnlalfjknjbanolhlnkmgp"
+          const CREDENTIALS_PATH = path.join(__dirname, "../credentials.json");
+          if (fs.existsSync(CREDENTIALS_PATH) && !!argv["release"]) {
+            console.log("Uploading to Chrome Store...");
+            const webStore = require("chrome-webstore-upload")({
+              ...require(CREDENTIALS_PATH),
+              extensionId: "emplcligcppnlalfjknjbanolhlnkmgp"
+            });
+
+            webStore
+              .fetchToken()
+              .then(token => {
+                return webStore
+                  .uploadExisting(fs.createReadStream(zipFileName), token)
+                  .then(res => {
+                    console.log("Uploaded to Chrome Store...Publishing");
+                    return webStore.publish("default", token);
+                  })
+                  .then(() =>
+                    console.log(`Published ${version} to Chrome Store successfully`)
+                  );
+              })
+              .catch(err => console.error(err));
+          }
         });
 
-        webStore
-          .fetchToken()
-          .then(token => {
-            return webStore
-              .uploadExisting(fs.createReadStream(zipFileName), token)
-              .then(res => {
-                console.log("Uploaded to Chrome Store...Publishing");
-                return webStore.publish("default", token);
-              })
-              .then(() =>
-                console.log(`Published ${version} to Chrome Store successfully`)
-              );
+        return resolve(
+          crx.pack(archiveBuffer).then(crxBuffer => {
+            return new Promise((resolve, reject) => {
+              fs.writeFile(crxFilename, crxBuffer, () => {
+                console.log(`Wrote ${crxFilename}`);
+                resolve();
+              });
+            });
           })
-          .catch(err => console.error(err));
-      }
+        );
+      });
     });
+  })
+  .then(() => {
+    if (!argv["release"]) {
+      return;
+    }
 
-    return crx.pack(archiveBuffer).then(async crxBuffer => {
-      const crxFilename = path.join(RELEASES_PATH, `${baseName}.crx`);
-      fs.writeFile(crxFilename, crxBuffer, () =>
-        console.log(`Wrote ${crxFilename}`)
-      );
+    const options = {
+      tag_name: `v${version}`,
+      version: version,
+      target_commitish: "master",
+      name: `v${version}`,
+      body: `New version`,
+      draft: false,
+      prerelease: false,
+      repo: "styleurl-extension",
+      owner: "Jarred-Sumner",
+      endpoint: "https://api.github.com",
+      auth: require(`/Users/${
+        process.env.USER
+      }/Library/Application Support/gh-release/config.json`),
+      assets: [`./releases/${baseName}.crx`, `./releases/${baseName}.zip`]
+    };
+
+    return new Promise((resolve, reject) => {
+      ghRelease(options, function(err, result) {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        resolve(result); // create release response: https://developer.github.com/v3/repos/releases/#response-4
+      });
     });
   })
   .catch(err => {
